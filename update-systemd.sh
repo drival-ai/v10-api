7731fbb1f4c3d66e179328175d5c3df8b2003045
flowerinthenight <root@flowerinthenight.com>
2025-03-20 12:55:25 +0900

    ci skip: remove the term SAMPLE_

--- update-systemd.sh
+++ update-systemd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SAMPLE_VERSION=$(curl -s https://api.github.com/repos/drival-ai/v10-mvp-api/releases/latest | jq -r ".tag_name")
-cd /tmp/ && wget https://github.com/drival-ai/v10-mvp-api/releases/download/$SAMPLE_VERSION/v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
-tar xvzf v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
+VERSION=$(curl -s https://api.github.com/repos/drival-ai/v10-mvp-api/releases/latest | jq -r ".tag_name")
+cd /tmp/ && wget https://github.com/drival-ai/v10-mvp-api/releases/download/$VERSION/v10-mvp-api-$VERSION-x86_64-linux.tar.gz
+tar xvzf v10-mvp-api-$VERSION-x86_64-linux.tar.gz
 sudo cp -v bin/v10-mvp-api /usr/local/bin/v10-mvp-api
 sudo chown root:root /usr/local/bin/v10-mvp-api
 
@@ -12,4 +12,4 @@ sudo systemctl start v10mvpapi
 sudo systemctl status v10mvpapi
 
 rm -rfv bin/
-rm v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
+rm v10-mvp-api-$VERSION-x86_64-linux.tar.gz
