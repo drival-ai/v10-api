b1e1dc0a1b8d461f275532bb3ee2f9bd40dc7aee
flowerinthenight <root@flowerinthenight.com>
2025-03-20 09:15:59 +0900

    ci skip: script for system service

--- update-systemd.sh
+++ update-systemd.sh
@@ -3,13 +3,13 @@
 SAMPLE_VERSION=$(curl -s https://api.github.com/repos/drival-ai/v10-mvp-api/releases/latest | jq -r ".tag_name")
 cd /tmp/ && wget https://github.com/drival-ai/v10-mvp-api/releases/download/$SAMPLE_VERSION/v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
 tar xvzf v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
-cp -v bin/v10-mvp-api /usr/local/bin/v10-mvp-api
-chown root:root /usr/local/bin/v10-mvp-api
+sudo cp -v bin/v10-mvp-api /usr/local/bin/v10-mvp-api
+sudo chown root:root /usr/local/bin/v10-mvp-api
 
-systemctl daemon-reload
-systemctl enable v10mvpapi
-systemctl start v10mvpapi
-systemctl status v10mvpapi
+sudo systemctl daemon-reload
+sudo systemctl enable v10mvpapi
+sudo systemctl start v10mvpapi
+sudo systemctl status v10mvpapi
 
 rm -rfv bin/
 rm v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
