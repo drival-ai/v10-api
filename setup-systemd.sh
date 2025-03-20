7b200d54896cfb0e3d29ec87b99c568d8b5b3503
flowerinthenight <root@flowerinthenight.com>
2025-03-20 09:03:42 +0900

    script for system service

--- setup-systemd.sh
+++ setup-systemd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SAMPLE_VERSION=$(curl -s https://api.github.com/repos/drival-al/v10-mvp-api/releases/latest | jq -r ".tag_name")
-cd /tmp/ && wget https://github.com/drival-al/v10-mvp-api/releases/download/$SAMPLE_VERSION/v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
+SAMPLE_VERSION=$(curl -s https://api.github.com/repos/drival-ai/v10-mvp-api/releases/latest | jq -r ".tag_name")
+cd /tmp/ && wget https://github.com/drival-ai/v10-mvp-api/releases/download/$SAMPLE_VERSION/v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
 tar xvzf v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
-cp -v v10-mvp-api /usr/local/bin/v10-mvp-api
+cp -v bin/v10-mvp-api /usr/local/bin/v10-mvp-api
 chown root:root /usr/local/bin/v10-mvp-api
 
 cat >/usr/lib/systemd/system/v10mvpapi.service <<EOL
@@ -24,3 +24,6 @@ systemctl daemon-reload
 systemctl enable v10mvpapi
 systemctl start v10mvpapi
 systemctl status v10mvpapi
+
+rm -rfv bin/
+rm v10-mvp-api-$SAMPLE_VERSION-x86_64-linux.tar.gz
